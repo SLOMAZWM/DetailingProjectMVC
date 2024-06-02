29c5fc69c7cde2c1207a28fdfcfd4c134b0947e2
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-06-02 23:29:53 +0200

    Visible/Hidden Cart

---
+++ wwwroot/js/cart.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var cartIcon = document.querySelector('.CartIcon');
+    var cartSummary = document.getElementById('cart-summary');
+    var closeCartButton = document.getElementById('close-cart');
+
+    cartIcon.addEventListener('click', function () {
+        cartSummary.classList.toggle('visible');
+    });
+
+    closeCartButton.addEventListener('click', function () {
+        cartSummary.classList.remove('visible');
+    });
+});
