3a4a6f44af1b8a59faecf2ba535965dfe96b0d8a
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-06-02 22:48:03 +0200

    Products view created, changed homeView

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjektLABDetailing.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektLABDetailing.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+    }
+}
